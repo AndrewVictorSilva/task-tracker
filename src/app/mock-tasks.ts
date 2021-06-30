f689c44e530a7a564ad7b49d00c27ab76f5f9419
Andrew Silva <avsilva.andrew@gmail.com>
2021-06-30 17:35:01 -0300

    Whole project

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: `lorem 1`,
+    day: 'Terça 3 às 15:30h',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: `lorem 2`,
+    day: 'Terça 7 às 17:30h',
+    reminder: false
+  },
+  {
+    id: 3,
+    text: `lorem 3`,
+    day: 'Terça 9 às 21:30h',
+    reminder: true
+  },
+  {
+    id: 4,
+    text: `lorem 4`,
+    day: 'Terça 15 às 14:30h',
+    reminder: false
+  },
+  {
+    id: 5,
+    text: `lorem 5`,
+    day: 'Terça 13 às 10:30h',
+    reminder: true
+  },
+]
